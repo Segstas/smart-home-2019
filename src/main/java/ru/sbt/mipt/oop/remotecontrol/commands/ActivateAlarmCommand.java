375d9f6e962ea38088b7b5c3e03012cf69360c34
Segstas <segstas@gmail.com>
2019-11-14 22:20:17 +0300

    Commands added

--- src/main/java/ru/sbt/mipt/oop/remotecontrol/commands/ActivateAlarmCommand.java
+++ src/main/java/ru/sbt/mipt/oop/remotecontrol/commands/ActivateAlarmCommand.java
@@ -1,9 +1,19 @@
 package ru.sbt.mipt.oop.remotecontrol.commands;
 
+import ru.sbt.mipt.oop.homeparts.SmartHome;
 import ru.sbt.mipt.oop.remotecontrol.Command;
 
 public class ActivateAlarmCommand implements Command {
+
+    private SmartHome smartHome;
+
+    public ActivateAlarmCommand(SmartHome smartHome) {
+        this.smartHome = smartHome;
+    }
+
+
     @Override
     public void execute() {
+        this.smartHome.getSignaling().triggerAlert();
     }
 }
--- src/main/java/ru/sbt/mipt/oop/remotecontrol/commands/EnableSignalingCommand.java
+++ src/main/java/ru/sbt/mipt/oop/remotecontrol/commands/EnableSignalingCommand.java
@@ -1,11 +1,19 @@
 package ru.sbt.mipt.oop.remotecontrol.commands;
 
 
+import ru.sbt.mipt.oop.homeparts.SmartHome;
 import ru.sbt.mipt.oop.remotecontrol.Command;
 
 public class EnableSignalingCommand implements Command {
+    private SmartHome smartHome;
+    private String code;
 
+    public EnableSignalingCommand(SmartHome smartHome, String code) {
+        this.smartHome = smartHome;
+        this.code = code;
+    }
     @Override
     public void execute() {
+        smartHome.getSignaling().activate(this.code);
     }
 }
